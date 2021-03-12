701973b5ae0a1b6af30d1633bb8b36c37d6ed612
Jose Moreno <alex@Joses-MacBook-Pro.local>
2021-03-11 21:47:19 -0500

    It's workinggit statusgit status

--- src/testTheGit/Realcoolname.java
+++ src/testTheGit/Realcoolname.java
@@ -5,11 +5,13 @@ public class Realcoolname {
 	public static void main(String[] args) {
 		
 		
-		some code the teacher wrote
-		random
-		about a pen
-		making it all the way
-
+		/*
+		 * some code the teacher wrote
+		 * random 
+		 * about a pen 
+		 * making it all the way
+		 */
+		
 	}
 
 }
