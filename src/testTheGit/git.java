3aabe64b84be3aa5cc89f8cb648c953fc7c01ce2
Jose Moreno <alex@Joses-MacBook-Pro.local>
2021-03-11 20:59:32 -0500

    lalalalala

--- src/testTheGit/git.java
+++ src/testTheGit/git.java
@@ -4,6 +4,9 @@ public class git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println(" we are learning git");
+		System.out.println("we git");
 
 	}
 
