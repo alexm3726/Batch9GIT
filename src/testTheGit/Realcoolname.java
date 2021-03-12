07ec1d3db0a84d884eb3ad952dcf5230a006d278
Jose Moreno <alex@Joses-MacBook-Pro.local>
2021-03-11 21:17:06 -0500

    this is it

---
+++ src/testTheGit/Realcoolname.java
@@ -0,0 +1,15 @@
+package testTheGit;
+
+public class Realcoolname {
+
+	public static void main(String[] args) {
+		
+		
+		some code the teacher wrote
+		random
+		about a pen
+		making it all the way
+
+	}
+
+}
